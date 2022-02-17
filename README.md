933cc83821398e7d3c9ec9251233fc1107c2e977
Alix Virot <flying.cat@mac.com>
2022-02-17 20:37:01 +0100

    changements noms fichiers

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GameProjectIH
+
+Index.html -> open with Live server
+
+bottom 34
+left 50
+
+À faire :
+
+- Qlqchose pour que l'on notice que la box s'est déplacée après qlq secondes.
+- Arrêter le Party Time
--- index.js
+++ index.js
@@ -1,19 +1,110 @@
+let h1Element = document.querySelector("h1");
+let selectColor = document.getElementById("clrSelect");
 
+selectColor.oninput = function () {
+  console.log(selectColor.value);
+  h1Element.style.backgroundColor = selectColor.value;
+};
 
-//////////////
-// let h1Element = document.querySelector('h1');
-// let colorOption = document.querySelectorAll("#clrSelect option");
-// console.log(colorOption);
-// colorOption.forEach(element => element.addEventListener("click",function(){
-//     h1Element.style.backgroundColor = colorOption.value;
-//     console.log(colorOption.parentElement);}))
- 
-  //  TEST 2: h1 en couleur 
-  let h1Element = document.querySelector('h1');
-  let selectColor = document.getElementById("clrSelect");
-  selectColor.oninput = function(){
-      console.log(selectColor.value);
-      h1Element.style.backgroundColor = selectColor.value;} 
+const superDiv = document.getElementById("superDiv");
+const userDiv = document.getElementById("userDiv");
+const winnerDiv = document.getElementById("winnerDiv");
+
+// USER DIV STYLE
+userDiv.style.position = "relative";
+userDiv.style.width = "80px";
+userDiv.style.height = "80px";
+userDiv.style.border = "2px ridge black";
+userDiv.style.backgroundColor = "#93f5cf";
+userDiv.style.opacity = "0.5";
+
+let selectPosition = document.getElementById("poposition");
+selectPosition.oninput = function () {
+  console.log("selectPosition.value: ", selectPosition.value);
+  userDiv.style.position = selectPosition.value;
+  checkPosition();
+};
+
+const btnTop = document.getElementById("btn-top");
+const inputTop = document.getElementById("top");
+function handleValueTop() {
+  console.log("inputTop.value ", inputTop.value);
+  userDiv.style.top = `${inputTop.value}px`;
+  checkPosition();
+}
+btnTop.addEventListener("click", handleValueTop);
+
+const btnRight = document.getElementById("btn-right");
+const inputRight = document.getElementById("right");
+function handleValueRight() {
+  userDiv.style.right = `${inputRight.value}px`;
+  checkPosition();
+}
+btnRight.addEventListener("click", handleValueRight);
+
+const btnBottom = document.getElementById("btn-bottom");
+const inputBottom = document.getElementById("bottom");
+function handleValueBottom() {
+  userDiv.style.bottom = `${inputBottom.value}px`;
+  checkPosition();
+}
+btnBottom.addEventListener("click", handleValueBottom);
+
+const btnLeft = document.getElementById("btn-left");
+const inputLeft = document.getElementById("left");
+function handleValueLeft() {
+  userDiv.style.left = `${inputLeft.value}px`;
+  checkPosition();
+}
+btnLeft.addEventListener("click", handleValueLeft);
+inputLeft.addEventListener("change", (event) => {
+  userDiv.style.left = `${event.target.value}px`;
+});
+
+// CODE CHANGEMENT DE COULEUR
+function generateRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function changeColor() {
+  userDiv.innerHTML = `<span>PARTY TIME BABY !!!</span>`;
+  // userDiv.innerHTML="PARTY TIME BABY !!!"
+  userDiv.style.opacity = "1";
+  userDiv.style.backgroundColor = generateRandomColor();
+}
+
+let intervalId;
+
+function checkPosition() {
+  let winnerDivPosition = winnerDiv.getBoundingClientRect();
+  let userDivPosition = userDiv.getBoundingClientRect();
+
+  if (
+    winnerDivPosition.x === userDivPosition.x &&
+    winnerDivPosition.y === userDivPosition.y
+  ) {
+    //userDiv.style.backgroundColor ="orange"; Fonctionne
+    intervalId = setInterval(changeColor, 500);
+    setTimeout(newRound, 3000);
+  } else {
+    userDiv.style.backgroundColor = "#93f5cf";
+    if (intervalId) {
+      clearInterval(intervalId);
+      userDiv.innerHTML = "";
+      userDiv.style.opacity = "0.5";
+    }
+  }
+}
+
+function newRound() {
+  console.log(winnerDiv);
+  let x = Math.floor(Math.random() * 420);
+  let y = Math.floor(Math.random() * 720);
+  console.log(x);
+  console.log(y);
+  winnerDiv.style.top = `${y}px`;
+  winnerDiv.style.left = `${x}px`;
+}
 
 // 0/ const recup element
 // ? / en quelle unité la value ?
@@ -30,106 +121,10 @@
 // superDiv.appendChild(userDiv);
 // userDiv.setAttribute("id","userDiv")
 
-const superDiv = document.getElementById("superDiv");
-const userDiv = document.getElementById("userDiv");
-const winnerDiv = document.getElementById("winnerDiv");
-
-userDiv.style.position = "relative";
-userDiv.style.width ="80px";
-userDiv.style.height ="80px";
-userDiv.style.border ="2px ridge black";
-userDiv.style.backgroundColor ="#93f5cf"; 
-userDiv.style.opacity ="0.5";
-
-
-let selectPosition = document.getElementById("poposition");
-selectPosition.oninput = function(){
-    console.log('selectPosition.value: ', selectPosition.value);
-    userDiv.style.position = selectPosition.value 
-    checkPosition();
-} 
-    
-const btnTop = document.getElementById("btn-top");
-const inputTop = document.getElementById("top");
-function handleValueTop(){
-    console.log('inputTop.value ', inputTop.value);
-    userDiv.style.top = `${inputTop.value}px`;
-    checkPosition();
-}
-btnTop.addEventListener("click",handleValueTop);
-
-const btnRight = document.getElementById("btn-right");
-const inputRight = document.getElementById("right");
-function handleValueRight(){
-    userDiv.style.right = `${inputRight.value}px`;
-    checkPosition();
-}
-btnRight.addEventListener("click",handleValueRight);
-
-const btnBottom = document.getElementById("btn-bottom");
-const inputBottom = document.getElementById("bottom");
-function handleValueBottom(){
-    userDiv.style.bottom = `${inputBottom.value}px`;
-    checkPosition();
-}
-btnBottom.addEventListener("click",handleValueBottom);
-
-const btnLeft = document.getElementById("btn-left");
-const inputLeft = document.getElementById("left");
-function handleValueLeft(){
-    userDiv.style.left = `${inputLeft.value}px`;
-    checkPosition();
-}
-btnLeft.addEventListener("click",handleValueLeft);
-inputLeft.addEventListener("change",(event)=>{
-    userDiv.style.left = `${event.target.value}px`;
-})
-
-
-function generateRandomColor() { 
-   return '#'+Math.floor(Math.random()*16777215).toString(16); 
-}
-
-function changeColor() { 
-    userDiv.innerHTML=`<span>PARTY TIME BABY !!!</span>`;
-    // userDiv.innerHTML="PARTY TIME BABY !!!"
-    userDiv.style.opacity ="1"
-    userDiv.style.backgroundColor = generateRandomColor(); 
-}
-
-let intervalId;
-
-function checkPosition(){
-    let winnerDivPosition = winnerDiv.getBoundingClientRect();
-    let userDivPosition = userDiv.getBoundingClientRect();
-
-    if(winnerDivPosition.x === userDivPosition.x && winnerDivPosition.y === userDivPosition.y){
-        //userDiv.style.backgroundColor ="orange"; Fonctionne 
-        intervalId = setInterval(changeColor, 500); 
-        setTimeout(newRound, 3000); 
-    }else{
-        userDiv.style.backgroundColor ="#93f5cf"; 
-    if (intervalId) {clearInterval(intervalId); 
-        userDiv.innerHTML="";
-        userDiv.style.opacity ="0.5";
-        }
-    };
-}
-
-function newRound(){
-    console.log(winnerDiv);
-    let x = Math.floor(Math.random() *420);
-    let y = Math.floor(Math.random() *720); 
-    console.log(x); 
-    console.log(y); 
-    winnerDiv.style.top =`${y}px`;
-    winnerDiv.style.left = `${x}px`;
-};
-
-
-
-
-
-
-
-
+//////////////
+// let h1Element = document.querySelector('h1');
+// let colorOption = document.querySelectorAll("#clrSelect option");
+// console.log(colorOption);
+// colorOption.forEach(element => element.addEventListener("click",function(){
+//     h1Element.style.backgroundColor = colorOption.value;
+//     console.log(colorOption.parentElement);}))
